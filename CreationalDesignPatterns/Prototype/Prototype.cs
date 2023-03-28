995911b8a1481cd7f12291096917d927fcae5ecc
Seher Dilek <sdilek@larsa4d.com>
2023-03-29 02:38:31 +0300

    Prototype

---
+++ CreationalDesignPatterns/Prototype/Prototype.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Prototype
+{
+    public interface IPrototype
+    {
+        public abstract IPrototype Clone();
+    }
+
+    public class ConcretePrototype : IPrototype
+    {
+        public object field1;
+
+        ConcretePrototype(ConcretePrototype prototype)
+        {
+            this.field1 = prototype.field1;
+        }
+
+        public override IPrototype Clone()
+        {
+            return new ConcretePrototype(this);
+        }
+    }
+
+    public class SubClassPrototype : ConcretePrototype
+    {
+        public object field2;
+
+        SubClassPrototype(SubClassPrototype prototype)
+        {
+            this.field2 = prototype.field2;
+        }
+
+        public override IPrototype Clone()
+        {
+            return new SubClassPrototype(this);
+        }
+    }
+}
