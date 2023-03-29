f7e74cdb3e0c589932ec446ce7dea5949866bb19
Seher Dilek <sdilek@larsa4d.com>
2023-03-30 01:53:56 +0300

    Singleton

---
+++ CreationalDesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        public static Singleton instance;
+        public readonly string field = "fieldOfSingletonClass";
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+
+    public class Program
+    {
+        public class Client
+        {
+            public void ClientCode()
+            {
+                var instance = Singleton.GetInstance();
+                Console.WriteLine(instance.field);
+            }
+        }
+        static void Main()
+        {
+            var client = new Client();
+            client.ClientCode();
+        }
+    }
+}
