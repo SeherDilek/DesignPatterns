a9ed8a9255e3541539cd8e66983759b3067cd822
Seher Dilek <sdilek@larsa4d.com>
2023-04-07 02:53:46 +0300

    Iterator

---
+++ BehavioralDesignPatterns/Iterator/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    public abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        public abstract object Current();
+
+        public abstract int Key();
+        public abstract bool MoveNext();
+
+        public abstract void Reset();
+    }
+
+    public abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+
+    public class AlphabeticalOrderIterator : Iterator
+    {
+        private WordsCollection _collection;
+        private int _position = -1;
+        private bool _reverse = false;
+
+        public AlphabeticalOrderIterator(WordsCollection collection, bool reverse = false)
+        {
+            this._collection = collection;
+            this._reverse = reverse;
+
+            if (reverse)
+            {
+                this._position = collection.GetItems().Count;
+            }
+        }
+
+        public override object Current()
+        {
+            return this._collection.GetItems()[_position];
+        }
+
+        public override int Key()
+        {
+            return this._position;
+        }
+
+        public override bool MoveNext()
+        {
+            int updatedPosition = this._position + (this._reverse ? -1 : 1);
+
+            if (updatedPosition >= 0 && updatedPosition < this._collection.GetItems().Count)
+            {
+                this._position = updatedPosition;
+                return true;
+            }
+            return false;
+        }
+
+        public override void Reset()
+        {
+            this._position = this._reverse ? this._collection.GetItems().Count - 1 : 0;
+        }
+    }
+
+    public class WordsCollection : IteratorAggregate
+    {
+        List<string> _collection = new List<string>();
+
+        bool _direction;
+
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+
+        public List<string> GetItems()
+        {
+            return _collection;
+        }
+
+        public void AddItem(string item)
+        {
+            _collection.Add(item);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new AlphabeticalOrderIterator(this, _direction);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code may or may not know about the Concrete Iterator
+            // or Collection classes, depending on the level of indirection you
+            // want to keep in your program.
+            var collection = new WordsCollection();
+            collection.AddItem("First");
+            collection.AddItem("Second");
+            collection.AddItem("Third");
+
+            Console.WriteLine("Straight traversal:");
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\nReverse traversal:");
+
+            collection.ReverseDirection();
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
